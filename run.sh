25f03cf9c3ca196ace4a100a086eb3a40bbd6611
ada <164679970+fahri3da@users.noreply.github.com>
2024-03-24 03:06:26 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-6fd1b278-ccc5-40f0-ae81-f46202d978a7/)
 
 
 echo "$curl_output" >> Update.tsx
